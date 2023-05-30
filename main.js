31af426886b6046b55e9b5515fda617cd2b870c3
Alicia Livingston <115050951+AliciaLivingston@users.noreply.github.com>
2023-05-30 15:27:45 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let typed = new Typed("#txt", {
+    strings: ["Backend Development", "Frontend Development", "Web Development"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
